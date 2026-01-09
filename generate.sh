13e5f5252dceffc51e7057a268674202cd767f36
Lawrence Renolds <lawrence.m.reynolds@googlemail.com>
2026-01-09 18:53:22 +0000

    Now generating full markdown file for reviewing.

--- generate.sh
+++ generate.sh
@@ -13,3 +13,15 @@ docker run --rm \
        -o ./output/Lawrence_Reynolds_CV_$(date -u +"%d%m%Y").docx \
        --data-dir=/data/templates \
        --reference-doc=/data/templates/custom-reference.docx
+
+docker run --rm \
+       --volume "$(pwd):/data" \
+       --user $(id -u):$(id -g) \
+       pandoc/core \
+       ./content/1_header.md \
+       ./content/2_Personal-details.md \
+       ./content/3_Profile.md \
+       ./content/4_Employment.md \
+       ./content/5_Education.md \
+       ./content/6_Interests-and-activities.md \
+       -o ./output/Lawrence_Reynolds_CV_$(date -u +"%d%m%Y").md 
